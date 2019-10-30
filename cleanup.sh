f52096968a7897a137b32ec5a67f1b1404493255
Lukas Bednar <lbednar@redhat.com>
2019-10-30 15:15:24 +0100

    Change CNV version
    Signed-off-by: Lukas Bednar <lbednar@redhat.com>

--- cleanup.sh
+++ cleanup.sh
@@ -3,6 +3,7 @@
 MARKETPLACE="${MARKETPLACE:-openshift-marketplace}"
 TARGET="${TARGET:-openshift-cnv}"
 APP_REGISTRY="${APP_REGISTRY:-rh-osbs-operators}"
+CNV_VERSION="${CNV_VERSION:-2.2.0}"
 
 oc delete csc hco-catalogsource-config -n $MARKETPLACE
 oc delete catalogsource $APP_REGISTRY -n $MARKETPLACE
@@ -10,6 +11,6 @@ oc delete operatorsource $APP_REGISTRY -n $MARKETPLACE
 oc delete hco hyperconverged-cluster -n $TARGET
 sleep 10
 oc delete sub hco-operatorhub -n $TARGET
-oc delete csv kubevirt-hyperconverged-operator.v2.1.0 kubevirt-hyperconverged-operator.v2.1.0-alpha -n $TARGET
+oc delete csv kubevirt-hyperconverged-operator.v${CNV_VERSION} kubevirt-hyperconverged-operator.v${CNV_VERSION}-alpha -n $TARGET
 oc delete operatorgroup $TARGET-group -n $TARGET
 oc delete secret $(oc get secret -n $MARKETPLACE | grep quay-registry | awk '{print $1}') -n $MARKETPLACE
